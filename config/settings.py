f2af01d693c72ce19b275fdcae1d694e150b9a29
haridharan <haridharan0311@gmail.com>
2026-03-31 11:47:34 +0530

    update the sample-output visuals

--- config/settings.py
+++ config/settings.py
@@ -5,15 +5,12 @@ from decouple import config, Csv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_!vt=j&&o1f!7_-mb)xlt)^%lt__$+2_k580)2@c_fiyjv*+4y'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # Allow all hosts for development
 
