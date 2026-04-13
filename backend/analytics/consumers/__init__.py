0a795bc1a07fa8a554c5e16eb52ab3de079da0be
haridharan <haridharan0311@gmail.com>
2026-04-13 15:52:16 +0530

    feat: reduce the files, process, to reduce the api response time

--- backend/analytics/consumers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .disease_consumer import DiseaseTrendConsumer
-from .spike_consumer import SpikeAlertConsumer
-from .restock_consumer import RestockConsumer
-
-__all__ = [
-    'DiseaseTrendConsumer',
-    'SpikeAlertConsumer',
-    'RestockConsumer'
-]
